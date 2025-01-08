987f24ad662b0e87d74e546b44fcc91e29f5f65e
Vinay Prakash Senapati <vinayprakashsenapati01@gmail.com>
2025-01-08 20:06:33 +0530

    loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -67,8 +67,6 @@ function App() {
       .catch((e) => console.log(e));
   };
 
-
-
   const EditHandler = (id) => {
     const task = todos.find((todo) => todo._id === id);
     setTask({
@@ -116,11 +114,15 @@ function App() {
 
   return (
     <>
-      <h1 className="Heading" ref={targetRef}>YearGoals</h1>
-      <p className="tagline1">Shout Your Goals to the World – Let's Make It Happen Together!</p>
+      <h1 className="Heading" ref={targetRef}>
+        YearGoals
+      </h1>
+      <p className="tagline1">
+        Shout Your Goals to the World – Let's Make It Happen Together!
+      </p>
       <p className="tagline2">No Turning Back,The World is Watching!</p>
       {/* form */}
-      <div className="form" >
+      <div className="form">
         <div className="input-group">
           <label className="input-label">Username:</label>
           <input
@@ -182,8 +184,111 @@ function App() {
       {/* goals */}
       <div className="todo-list">
         {todos.length === 0 ? (
-          <div className="no-records">
-            <h2>No Records</h2>
+          <div className="Loading">
+            <svg
+              version="1.1"
+              id="L1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 100 100"
+              enable-background="new 0 0 100 100"
+              xml:space="preserve"
+            >
+              <circle
+                fill="none"
+                stroke="#e76427"
+                stroke-width="6"
+                stroke-miterlimit="15"
+                stroke-dasharray="14.2472,14.2472"
+                cx="50"
+                cy="50"
+                r="47"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  attributeType="XML"
+                  type="rotate"
+                  dur="5s"
+                  from="0 50 50"
+                  to="360 50 50"
+                  repeatCount="indefinite"
+                />
+              </circle>
+              <circle
+                fill="none"
+                stroke="#e76427"
+                stroke-width="1"
+                stroke-miterlimit="10"
+                stroke-dasharray="10,10"
+                cx="50"
+                cy="50"
+                r="39"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  attributeType="XML"
+                  type="rotate"
+                  dur="5s"
+                  from="0 50 50"
+                  to="-360 50 50"
+                  repeatCount="indefinite"
+                />
+              </circle>
+              <g fill="#e76427">
+                <rect x="30" y="35" width="5" height="30">
+                  <animateTransform
+                    attributeName="transform"
+                    dur="1s"
+                    type="translate"
+                    values="0 5 ; 0 -5; 0 5"
+                    repeatCount="indefinite"
+                    begin="0.1"
+                  />
+                </rect>
+                <rect x="40" y="35" width="5" height="30">
+                  <animateTransform
+                    attributeName="transform"
+                    dur="1s"
+                    type="translate"
+                    values="0 5 ; 0 -5; 0 5"
+                    repeatCount="indefinite"
+                    begin="0.2"
+                  />
+                </rect>
+                <rect x="50" y="35" width="5" height="30">
+                  <animateTransform
+                    attributeName="transform"
+                    dur="1s"
+                    type="translate"
+                    values="0 5 ; 0 -5; 0 5"
+                    repeatCount="indefinite"
+                    begin="0.3"
+                  />
+                </rect>
+                <rect x="60" y="35" width="5" height="30">
+                  <animateTransform
+                    attributeName="transform"
+                    dur="1s"
+                    type="translate"
+                    values="0 5 ; 0 -5; 0 5"
+                    repeatCount="indefinite"
+                    begin="0.4"
+                  />
+                </rect>
+                <rect x="70" y="35" width="5" height="30">
+                  <animateTransform
+                    attributeName="transform"
+                    dur="1s"
+                    type="translate"
+                    values="0 5 ; 0 -5; 0 5"
+                    repeatCount="indefinite"
+                    begin="0.5"
+                  />
+                </rect>
+              </g>
+            </svg>
           </div>
         ) : (
           todos.map((todo) => (
@@ -197,7 +302,7 @@ function App() {
                 Added on: {todo.addedon}
               </div>
               <div className="todo-detail dedline">
-                Deadline: {new Date(todo.dedline).toISOString().split('T')[0]}
+                Deadline: {new Date(todo.dedline).toISOString().split("T")[0]}
               </div>
               <div className="todo-detail username">{todo.username}</div>
               <div className="todo-actions">
